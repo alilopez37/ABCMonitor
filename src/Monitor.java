da2681aed96cc385dd0aa63fb5e2c4c6b0fcb9cd
alilopez <alzunun@upchiapas.edu.mx>
2021-04-12 11:41:56 -0500

    Add Monitor

--- src/Monitor.java
+++ src/Monitor.java
@@ -4,30 +4,47 @@ public class Monitor {
         private boolean b = false;
         private boolean c = false;
         Object objeto = new Object();
+        String objetoString = "null";
 
-        public synchronized void imprimirA(){
-            while (!a)   //GuardaBoolena
-                try { this.wait();}
-                catch(Exception e) {}
-            System.out.println(Thread.currentThread().getName() +": A");
-            b = true;
-            this.notifyAll();
-        }
-        public synchronized void imprimirB(){
-            while (!b)
-                try { this.wait();}
-                catch(Exception e) {}
-            System.out.println(Thread.currentThread().getName() +": B");
-            c = true;
-            this.notifyAll();
-        }
-        public  void imprimirC(){
-            synchronized (objeto) {
-                while (!c)
+        public  synchronized void imprimirA(){
+                while (!a)   //GuardaBoolena
                     try {
-                        objeto.wait();
+                        this.wait();
                     } catch (Exception e) {
                     }
+                System.out.println(Thread.currentThread().getName() + ": A");
+                b = true;
+                this.notifyAll();
+        }
+
+        public  void imprimirB(){
+            synchronized (this) {
+                while (!b)
+                    try {
+                        this.wait();
+                    } catch (Exception e) {
+                    }
+                System.out.println(Thread.currentThread().getName() + ": B");
+                c = true;
+
+                synchronized (objetoString) {
+                    objetoString.notifyAll();
+                }
+            }
+        }
+
+
+        public  void imprimirC(){
+            synchronized (objetoString) {
+                System.out.println("Entrando a C");
+                while (!c) {
+                    try {
+                        objetoString.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
                 System.out.println(Thread.currentThread().getName() + ": C");
             }
         }
